6ba63561f3c06a69bf8d0eeeda9a0befd70ac239
Weitian Leung <weitianleung@gmail.com>
2016-03-09 18:44:37 +0800

    Fixed unable change wallpaper
    Now set to every monitor that contains image-path or last-image.
    It seems that xfdesktop not always use the "connected" one.

--- xfce4_set_wallpaper.sh
+++ xfce4_set_wallpaper.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Author: Weitian Leung <weitianleung@gmail.com>
-# Version: 1.0
+# Version: 2.0
 # License: GPL-3.0
 # Description: set a picture as xfce4 wallpaper
 
-tools=(xrandr xfconf-query)
+tools=(xfconf-query)
 
 for tool in ${tools[@]} ; do
 	if ! which $tool &> /dev/null ; then
@@ -23,18 +23,8 @@ if [[ ! "$mime_type" == image/* ]]; then
 	exit 1
 fi
 
-# TODO: when monitor name is NULL, changed to number
-monitor=($(xrandr | grep " connected" | cut -d' ' -f1))
-
-if [ ${#monitor[@]} -gt 1 ] ; then
-	exp="\("
-	for m in ${monitor[@]} ; do
-		exp+="$m\|"
-	done
-	monitor="${exp::-2}\)"
-fi
-
-properties=$(xfconf-query -c xfce4-desktop -p /backdrop -l | grep -e "screen.*/monitor${monitor}.*image-path$" -e "screen.*/monitor${monitor}.*/last-image$")
+# set to every monitor that contains image-path/last-image
+properties=$(xfconf-query -c xfce4-desktop -p /backdrop -l | grep -e "screen.*/monitor.*image-path$" -e "screen.*/monitor.*/last-image$")
 
 for property in $properties; do
 	xfconf-query -c xfce4-desktop -p $property -s "$wallpaper"
